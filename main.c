dc34a6704f9bf3425cd407ee5ef8454b7f32602b
Frank Tkalcevic <frank@franksworkshop.com.au>
2018-07-05 10:25:49 +1000

    Initial commit to capture original atmel USI_TWI_Slave files

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * 1sXBusAltimeter.c
+ *
+ * Created: 5/07/2018 10:19:35 AM
+ * Author : Frank Tkalcevic
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
